2b1a06eb516cdc6990411281450eef508c628be6
alkh0115 <alkh0115@algonquinlive.com>
2025-07-23 13:07:40 -0400

    Added AKS infrastructure configuration

--- infra/tf-app/main.tf
+++ infra/tf-app/main.tf
@@ -5,6 +5,35 @@ terraform {
       version = "~> 3.75.0"
     }
   }
-
   required_version = ">= 1.5.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "aks_rg" {
+  name     = "aks-rg"
+  location = var.location
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "aks-cluster"
+  location            = azurerm_resource_group.aks_rg.location
+  resource_group_name = azurerm_resource_group.aks_rg.name
+  dns_prefix          = "akscluster"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    environment = "dev"
+  }
 }
