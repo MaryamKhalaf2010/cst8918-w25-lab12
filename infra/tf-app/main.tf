cd772be8702f14894a41fc99c242c5d64076b86e
MteacherIT <m.adwan23@hotmail.com>
2025-07-24 00:29:24 -0400

    Enable OIDC and update resource group config

--- infra/tf-app/main.tf
+++ infra/tf-app/main.tf
@@ -1,42 +1,4 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.75.0"
-    }
-  }
-  required_version = ">= 1.5.0"
+resource "azurerm_resource_group" "app" {
+  name     = "khal0233-a12-rg"
+  location = "canadaeast"
 }
-
-provider "azurerm" {
-  features {}
-}
-
-resource "azurerm_resource_group" "aks_rg" {
-  name     = "aks-rg"
-  location = var.location
-}
-
-resource "azurerm_kubernetes_cluster" "aks" {
-  name                = "aks-cluster"
-  location            = azurerm_resource_group.aks_rg.location
-  resource_group_name = azurerm_resource_group.aks_rg.name
-  dns_prefix          = "akscluster"
-
-  default_node_pool {
-    name       = "default"
-    node_count = 1
-    vm_size    = "Standard_DS2_v2"
-  }
-
-  identity {
-    type = "SystemAssigned"
-  }
-
-  tags = {
-    environment = "dev"
-  }
-}
-
-
-# Trigger CI rerun
