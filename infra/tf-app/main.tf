306b67fa2ea0ac8cd27271285b2f68c49cac6456
MteacherIT <m.adwan23@hotmail.com>
2025-07-24 01:32:31 -0400

    Add OIDC authentication variables

--- infra/tf-app/main.tf
+++ infra/tf-app/main.tf
@@ -2,3 +2,11 @@ resource "azurerm_resource_group" "app" {
   name     = "khal0233-a12-rg"
   location = "canadaeast"
 }
+provider "azurerm" {
+  features {}
+
+  use_oidc        = true
+  tenant_id       = var.tenant_id
+  client_id       = var.client_id
+  subscription_id = var.subscription_id
+}
