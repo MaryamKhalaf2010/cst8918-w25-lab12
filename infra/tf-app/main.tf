7a38150c0b6db4954adb9533bfb04ba8cbcbff74
MteacherIT <m.adwan23@hotmail.com>
2025-07-24 00:51:46 -0400

    remove location

--- infra/tf-app/main.tf
+++ infra/tf-app/main.tf
@@ -1,4 +1,4 @@
 resource "azurerm_resource_group" "app" {
   name     = "khal0233-a12-rg"
-  location = var.location
+  location = "canadaeast"
 }
--- infra/tf-app/variables.tf
+++ infra/tf-app/variables.tf
@@ -1,4 +1,1 @@
-variable "location" {
-  type    = string
-  default = "eastus"
-}
+
