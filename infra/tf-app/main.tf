b9359b0bf1e98ce4bcaaf245a2a736044a354a48
MteacherIT <m.adwan23@hotmail.com>
2025-07-29 19:44:04 -0400

    Add NSG and associate with subnet to fix Checkov error

--- infra/tf-app/main.tf
+++ infra/tf-app/main.tf
@@ -16,3 +16,16 @@ resource "azurerm_subnet" "app_subnet" {
   virtual_network_name = azurerm_virtual_network.app_vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
+
+# ✅ Add a Network Security Group
+resource "azurerm_network_security_group" "app_nsg" {
+  name                = "khal0233-a12-nsg"
+  location            = azurerm_resource_group.app_rg.location
+  resource_group_name = azurerm_resource_group.app_rg.name
+}
+
+# ✅ Associate NSG with Subnet
+resource "azurerm_subnet_network_security_group_association" "app_subnet_nsg_assoc" {
+  subnet_id                 = azurerm_subnet.app_subnet.id
+  network_security_group_id = azurerm_network_security_group.app_nsg.id
+}
